75cdc011b17a357a732a46a452225887ab73ca8a
jenniferwvng <83246937+jenniferwvng@users.noreply.github.com>
2022-06-08 23:28:17 +0200

    Display all tasks for given user on frontend

--- frontend/src/components/Weekly.js
+++ frontend/src/components/Weekly.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect ,useState } from 'react';
 
 const Weekly = () => {
+  const [jsonRes, setJsonRes] = useState([]);
+
+  useEffect(() => {
+    const getUserTasks = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/gettask');
+        const allTasks = await response.json();
+        const taskArray = allTasks.selectTask.tasks;
+        setJsonRes(taskArray);
+      } catch(err) {
+          console.error(err);
+      }
+    }
+    getUserTasks();
+  }, []);
+
+  if(!jsonRes.length) {
+    return (
+      <>
+        <p>No weekly tasks set - add some</p>
+      </>
+    )
+  }
+
   return (
     <div>
       <h1>Weekly page</h1>
+      {jsonRes.map(item => {
+        return (
+          <div style={{display: 'flex'}}>
+          <p style={{margin: '10px'}}>Task: {item.name}</p>
+          <p style={{margin: '10px'}}>Date: {item.date}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
