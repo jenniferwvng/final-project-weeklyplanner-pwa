6092de907d2788f8da95d92cf1f9b30c750159a4
jenniferwvng <83246937+jenniferwvng@users.noreply.github.com>
2022-06-12 20:35:58 +0200

    Basic conditional rendering based on weekday, before refactoring

--- frontend/src/components/Weekly.js
+++ frontend/src/components/Weekly.js
@@ -45,19 +45,48 @@ const Weekly = () => {
   )
   }
 
+   const nameOfWeekdays = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun'];
+
+  const loopOverWeekdays = () => {
+    let test = '';
+    for (let i = 0; i < nameOfWeekdays.length; i++) {
+       console.log(nameOfWeekdays[i]);
+    }
+    
+  }
+
+  console.log(loopOverWeekdays())
+
   return (
     <div>
       <h1>Weekly page</h1>
       <AddtaskForm />
       {jsonRes.map(item => {
-        return (
-          <div style={{display: 'flex'}}>
-          <p style={{margin: '10px'}}>Task: {item.name}</p>
-          <p style={{margin: '10px'}}>Date: {item.date}</p>
-          <p style={{margin: '10px'}}>ID: {item._id}</p>
-          <button onClick={() => deleteTaskById(item._id)}>Delete task</button>
-          </div>
-        )
+        //somehow loop through and generate new weekdays to compare in .includes w/o having to hardcode every string as it is now
+        if (item.date.includes('mon')) {
+          return (
+            <div style={{backgroundColor: 'pink'}}>
+              <h1>mon</h1>
+              <p style={{margin: '10px'}}>Task: {item.name}</p>
+              <p style={{margin: '10px'}}>Date: {item.date}</p>
+              <p style={{margin: '10px'}}>ID: {item._id}</p>
+              <button onClick={() => deleteTaskById(item._id)}>Delete task</button>
+            </div>
+          )
+        } else {
+          return (
+            //if item.date === contains mon, render inside div with h1 monday, else if item.date contains tues, render inside div w... etc.
+            //could be flex divs with flexdirection column, left to right, to make space for more tasks
+            <div style={{display: 'flex'}}>
+            <p style={{margin: '10px'}}>Task: {item.name}</p>
+            <p style={{margin: '10px'}}>Date: {item.date}</p>
+            <p style={{margin: '10px'}}>ID: {item._id}</p>
+            <button onClick={() => deleteTaskById(item._id)}>Delete task</button>
+            </div>
+          )
+        }
+      //})
+      
       })}
     </div>
   );
