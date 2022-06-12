1c6eead7335f7d88fde716cfb5cd1de24a8222ef
jenniferwvng <83246937+jenniferwvng@users.noreply.github.com>
2022-06-12 19:53:11 +0200

    Generating upcoming weekdays and days user form n database input

--- frontend/src/components/AddtaskForm.js
+++ frontend/src/components/AddtaskForm.js
@@ -1,26 +1,52 @@
 import React, { useState } from 'react';
 
+
+const weekdays = new Map();
+weekdays.set(0, 'sun');
+weekdays.set(1, 'mon');
+weekdays.set(2, 'tues');
+weekdays.set(3, 'wed');
+weekdays.set(4, 'thurs');
+weekdays.set(5, 'fri');
+weekdays.set(6, 'sat');
+
+const todaysDate = new Date().toLocaleDateString();
+const todaysWeekday = weekdays.get(new Date().getDay());
+
+const dateOfToday = todaysWeekday + ' ' + todaysDate;
+
+const addUpcomingDates = (numOfDays) => {
+    return new Date(new Date(new Date()).setDate(new Date().getDate() + numOfDays)).toLocaleDateString();
+}
+
+const addWeekdays = (numOfDays) => {
+    return new Date(new Date(new Date(new Date()).setDate(new Date().getDate() + numOfDays))).getDay();
+}
+
 const AddtaskForm = () => {
     const [name, setName] = useState('');
-    const [date, setDate] = useState(0);
+    const [date, setDate] = useState(dateOfToday);
     const [done, setDone] = useState(false);
-
+    const [addedTask, setAddedTask] = useState([]);
+ 
     const AddTask = async (e) => {
         e.preventDefault();
-        
+
         try {
-            await fetch('http://localhost:8080/addtask', {
+            const addAction = await fetch('http://localhost:8080/addtask', {
               method: 'POST',
               headers: {
                'Content-type': 'application/json; charset=UTF-8' 
               },
               body: JSON.stringify({name, date, done})
             });
+            const addResponse = await addAction.json();
+            setAddedTask(addResponse);
           } catch(err) {
               console.error(err);
           }
     }
-
+    
   return (
       <>
     <form onSubmit={AddTask}>
@@ -34,11 +60,15 @@ const AddtaskForm = () => {
     </label>
     <label>
       Date to be done
-      <input 
-        type="text" 
-        value={date} 
-        onChange={(e) => setDate(e.target.value)} 
-      />
+        <select  onChange={(e) => setDate(e.target.value)}>
+        <option value={dateOfToday}>Today</option>
+        <option value={weekdays.get(addWeekdays(1)) + ' ' + addUpcomingDates(1)}> {weekdays.get(addWeekdays(1)) + ' ' + addUpcomingDates(1)} </option>
+        <option value={weekdays.get(addWeekdays(2)) + ' ' + addUpcomingDates(2)}> {weekdays.get(addWeekdays(2)) + ' ' + addUpcomingDates(2)} </option>
+        <option value={weekdays.get(addWeekdays(3)) + ' ' + addUpcomingDates(3)}> {weekdays.get(addWeekdays(3)) + ' ' + addUpcomingDates(3)} </option>
+        <option value={weekdays.get(addWeekdays(4)) + ' ' + addUpcomingDates(4)}> {weekdays.get(addWeekdays(4)) + ' ' + addUpcomingDates(4)} </option>
+        <option value={weekdays.get(addWeekdays(5)) + ' ' + addUpcomingDates(5)}> {weekdays.get(addWeekdays(5)) + ' ' + addUpcomingDates(5)} </option>
+        <option value={weekdays.get(addWeekdays(6)) + ' ' + addUpcomingDates(6)}> {weekdays.get(addWeekdays(6)) + ' ' + addUpcomingDates(6)} </option>
+        </select>
     </label>
     <label>
       Done
